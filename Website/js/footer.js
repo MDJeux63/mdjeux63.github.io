f0750ea3f38502c41f0ff8276efd00b2a3554b2c
MDJeux63 <147134929+MDJeux63@users.noreply.github.com>
2023-12-06 11:16:34 +0100

    Add files via upload

---
+++ Website/js/footer.js
@@ -0,0 +1,6 @@
+document.write(`
+<footer>
+        <p>&copy; 2023-2024 &ndash; MDJeux63 &ndash; Tous droits réservés.</p>
+        <p class="legal-link"><a href="../fr-fr/legal.html">Mentions légales, Politique de confidentialité & Conditions générales de vente</a></p>
+</footer>
+`);
