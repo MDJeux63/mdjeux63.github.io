a18a7da1e2e8004489c19082e157cb1346a987f7
MDJeux63 <147134929+MDJeux63@users.noreply.github.com>
2023-12-27 18:11:07 +0100

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -7,7 +7,7 @@ document.write(`
                 <h1 class="nav-logo">MDJeux63</h1>
             </div>
             <ul>
-                <li><a href="../fr-fr/home.html">Accueil</a></li>
+                <li><a href="../fr-fr/home.html#">Accueil</a></li>
                 <li><a href="../fr-fr/home.html#about">À propos</a></li>
                 <li><a href="../fr-fr/home.html#games">Nos jeux</a></li>
                 <li><a href="../fr-fr/home.html#order">Commander</a></li>
