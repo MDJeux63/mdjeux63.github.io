249155d022c19e97ff2c9e394cd23d855999c1a9
MDJeux63 <147134929+MDJeux63@users.noreply.github.com>
2023-12-28 19:37:54 +0100

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -1,21 +1,29 @@
 document.write(`
-<header>
-        <nav id="nav">
-            <div class="nav-icon-logo">
-                <img src="/assets/media/nav-icon.png" alt="Icône" class="nav-icon nav-icon-unhover">
-                <img src="/assets/media/nav-icon-blue.png" alt="Icône" class="nav-icon nav-icon-hover">
-                <h1 class="nav-logo">MDJeux63</h1>
-            </div>
-            <ul>
-                <li><a href="/fr-fr/home.html#">Accueil</a></li>
-                <li><a href="/fr-fr/home.html#about">À propos</a></li>
-                <li><a href="/fr-fr/home.html#games">Nos jeux</a></li>
-                <li><a href="/fr-fr/home.html#order">Commander</a></li>
-                <li><a href="/fr-fr/home.html#join">Nous rejoindre</a></li>
-            </ul>
-            <div id="icons"></div>
-        </nav>
-
-        <script src="/js/nav.js"></script>
-</header>
+<nav id="nav">
+    <div class="nav-icon-logo">
+        <img src="/assets/media/nav-icon.png" alt="Icône" class="nav-icon nav-icon-unhover">
+        <img src="/assets/media/nav-icon-blue.png" alt="Icône" class="nav-icon nav-icon-hover">
+        <h1 class="nav-logo">MDJeux63</h1>
+    </div>
+    <ul>
+        <li><a href="/fr-fr/home.html#">Accueil</a></li>
+        <li><a href="/fr-fr/home.html#about">À propos</a></li>
+        <li><a href="/fr-fr/home.html#games">Nos jeux</a></li>
+        <li><a href="/fr-fr/home.html#order">Commander</a></li>
+        <li><a href="/fr-fr/home.html#join">Nous rejoindre</a></li>
+    </ul>
+    <div id="icons"></div>
+</nav>
 `);
+
+var links = document.querySelectorAll("nav ul li");
+
+icons.addEventListener("click", () => {
+  nav.classList.toggle("nav-mobile");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("nav-mobile");
+  });
+});
