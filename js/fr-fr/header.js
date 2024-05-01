fb98c1469a71f38844a31825168207934b6e219c
MDJeux63 <147134929+MDJeux63@users.noreply.github.com>
2024-05-01 15:16:20 +0200

    Add files via upload

--- js/fr-fr/header.js
+++ js/fr-fr/header.js
@@ -1,17 +1,14 @@
 document.write(`
 <nav id="nav">
-    <div class="nav-icon-logo">
-        <img src="/assets/media/nav-icon.png" alt="Icône" class="nav-icon nav-icon-unhover">
-        <img src="/assets/media/nav-icon-blue.png" alt="Icône" class="nav-icon nav-icon-hover">
-        <h1 class="nav-logo">MDJeux63</h1>
-    </div>
+    <h1 class="nav-logo nav-logo-1">MDJeux63</h1>
+    <h1 class="nav-logo nav-logo-2">MD</h1>
     <ul>
         <li><a href="/fr-fr.html#">Accueil</a></li>
         <li><a href="/fr-fr.html#about">À propos</a></li>
         <li><a href="/fr-fr.html#games">Nos jeux</a></li>
         <li><a href="/fr-fr.html#order">Commander</a></li>
+        <li><a href="/fr-fr.html#videos">Nos vidéos</a></li>
         <li><a href="/fr-fr.html#join">Nous rejoindre</a></li>
-        <li class="nav-separator"></li>
         <li class="nav-special-item"><a href="/fr-fr/mdstock.html">MDStock</a></li>
     </ul>
     <div id="icons"></div>
