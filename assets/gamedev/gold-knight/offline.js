5ba6a1c64bbc57c9f7beef535493d106fa99ab35
MDJeux63 <147134929+MDJeux63@users.noreply.github.com>
2024-05-08 12:48:10 +0200

    Add files via upload

---
+++ assets/gamedev/gold-knight/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1715161515,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/knight-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/trigger-sheet0.png",
+		"images/particles.png",
+		"media/collectpoints.m4a",
+		"media/collectpoints.ogg",
+		"media/gameover.m4a",
+		"media/gameover.ogg",
+		"media/music.m4a",
+		"media/music.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
