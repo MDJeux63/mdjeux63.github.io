e8e4357e2e181d70861b33136e27efb96a126a6b
MDJeux63 <147134929+MDJeux63@users.noreply.github.com>
2024-05-18 14:50:22 +0200

    Add files via upload

---
+++ fr-fr/gamedev/invasion-z/offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1716031655,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/spawnzombies-sheet0.png",
+		"images/zombie1-sheet0.png",
+		"images/zombie2-sheet0.png",
+		"images/cartouche-sheet0.png",
+		"images/spawnballes-sheet0.png",
+		"images/viseur-sheet0.png",
+		"images/trigger-sheet0.png",
+		"images/particles.png",
+		"images/particles2.png",
+		"images/playbutton-sheet0.png",
+		"images/sprite-sheet0.png",
+		"media/pistol.m4a",
+		"media/pistol.ogg",
+		"media/1911-reload-6248.m4a",
+		"media/1911-reload-6248.ogg",
+		"media/music.m4a",
+		"media/music.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
