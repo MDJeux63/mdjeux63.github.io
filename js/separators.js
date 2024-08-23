c5a338efe48de6f4d8c6070aea48e7d51c9f95c7
unknown <rafdeoliveira27@gmail.com>
2024-08-23 11:00:47 +0200

    Update of 23.08.2024 – Part 2

---
+++ js/separators.js
@@ -0,0 +1,6 @@
+document.write(`
+    
+    <img src="/assets/images/separators/separator-1-desktop.png" alt="" class="separator-1 separator-desktop">
+    <img src="/assets/images/separators/separator-1-mobile.png" alt="" class="separator-1 separator-mobile">
+    
+    `);
