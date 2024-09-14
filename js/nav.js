776c914e87dc965b2592f224bac8f93589673b56
MrRafael27 <rafdeoliveira27@gmail.com>
2024-09-14 07:43:04 +0200

    Update of 14.09.2024

---
+++ js/nav.js
@@ -0,0 +1,18 @@
+document.write(`
+    
+    <nav>
+        <a href="/index.html#" class="nav-title">MDJeux63</a>
+        <div class="nav-links">
+            <a href="/index.html#a-propos">À propos</a>
+            <a href="/index.html#jeux-de-societe">Jeux de société</a>
+            <a href="/index.html#commander">Commander</a>
+            <a href="/index.html#gamedev">Gamedev</a>
+            <a href="/index.html#mdstock">MDStock</a>
+            <a href="/index.html#nos-videos">Nos vidéos</a>
+            <a href="/index.html#nous-rejoindre">Nous rejoindre</a>
+            <a href="/index.html#myths-and-legends">Myths and Legends</a>
+            <a href="/index.html#rafael-studios">Rafaël Studios</a>
+        </div>
+    </nav>
+    
+    `);
