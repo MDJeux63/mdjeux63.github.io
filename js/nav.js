8aa5a68b6e38ede14077998a88ae96d8f6e0282f
MrRafael27 <rafdeoliveira27@gmail.com>
2024-09-24 18:10:07 +0200

    Update of 24.09.2024

--- js/nav.js
+++ js/nav.js
@@ -6,10 +6,9 @@ document.write(`
             <a href="/index.html#a-propos">À propos</a>
             <a href="/index.html#jeux-de-societe">Jeux de société</a>
             <a href="/index.html#commander">Commander</a>
-            <a href="/index.html#gamedev">Gamedev</a>
-            <a href="/index.html#mdstock">MDStock</a>
             <a href="/index.html#nos-videos">Nos vidéos</a>
             <a href="/index.html#nous-rejoindre">Nous rejoindre</a>
+            <a href="/index.html#mdstock">MDStock</a>
             <a href="/index.html#myths-and-legends">Myths and Legends</a>
         </div>
     </nav>
