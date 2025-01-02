50b285bb04ed15323f390fe60c5c059380a04b42
MrRafael27 <blue1.mrrafael27@proton.me>
2025-01-02 14:24:16 +0100

    Update of 02.01.2025 – Part 2

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,7 @@
 document.write(`
     
     <footer>
-        <p>&copy; 2024 &ndash; MDJeux63 &ndash; Tous droits réservés.</p>
+        <p>&copy; 2025 &ndash; MDJeux63 &ndash; Tous droits réservés.</p>
         <p><a href="/legal.html">Mentions légales</a> &ndash; <a href="https://drive.google.com/uc?id=1e3azNr3XB6i-qXjx6N4hs9aPvs5eGuE-&export=download" target="_blank">Dossier de presse</a></p>
         <p>Toutes les marques citées appartiennent à leurs propriétaires respectifs.</p>
     </footer>
